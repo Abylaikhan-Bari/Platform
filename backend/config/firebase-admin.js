70a0a11652e5e471900ae0ce300502c7ce5011bf
Abylaikhan Bari <aikei@Abylaikhans-MacBook-Pro.local>
2025-02-11 23:11:14 +0500

    minor update

--- backend/config/firebase-admin.js
+++ backend/config/firebase-admin.js
@@ -1,7 +1,7 @@
 const admin = require("firebase-admin");
 
 admin.initializeApp({
-    credential: admin.credential.cert(require('../config/platform-780bc-firebase-adminsdk-fbsvc-3fd79abd38.json')),  // Correct path to your Firebase service account JSON file
+    credential: admin.credential.cert(require(process.env.FIREBASE_ADMIN_SDK)),  // Correct path to your Firebase service account JSON file
 });
 
 module.exports = admin;
